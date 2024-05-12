9ede72e36d100f7dbf5958ce30863ed050177d5b
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-05-12 11:24:51 +0200

    To się chyba łączy

---
+++ src/php/box.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <title>Szatnia</title>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+    
+    <?php
+
+        require_once("connection.php");
+
+        $connection = @new mysqli($host, $user, $password, $db);
+
+    ?>
+
+</body>
+</html>
