17a5c880bfe6db2ad6d55d7bd7f1d8dd662436ba
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-05-19 17:52:35 +0200

    To tak się robi kwerendy i tabele

--- src/php/room.php
+++ src/php/room.php
@@ -14,6 +14,31 @@
 
         $connection = @new mysqli($host, $user, $password, $db);
 
+        $room = $_POST["roomInput"];
+
+        $sql = "SELECT box.* FROM box, room WHERE box.room_id=room.id AND room.id=$room";
+        $result = $connection->query($sql);
+
+        echo "<h2>Pomieszczenie $room</h2>";
+
+        if ($result->num_rows > 0)
+        {
+            echo "<table>";
+                echo "<tr>";
+                    echo "<th>Numer</th>";
+                    echo "<th>Dostępność</th>";
+                echo "</tr>";
+
+            while ($row = $result->fetch_assoc())
+            {
+                echo "<tr>";
+                    echo "<td>" . $row["id"] . "</td>"; //Pytanie czy jak traktujemy liczenie przedziałów z rooma drugiego
+                    echo "<td>" . ($row["accesability"] ? "Dostępny" : "Niedostępny") . "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        }
     ?>
 
 </body>
